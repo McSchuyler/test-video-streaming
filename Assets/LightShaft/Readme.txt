088813334b0a7a381bc3d1b110fa0f043d208113
SchuylerChew <speedgolden@gmail.com>
2018-05-02 12:48:09 +0800

    Init

---
+++ Assets/LightShaft/Readme.txt
@@ -0,0 +1,8 @@
+If you receive errors remeber that you need the easy movie texture asset:
+https://www.assetstore.unity3d.com/en/#!/content/10032
+
+Unity have plans to add HLS support so, in the future you will dont need easy movie texture, but by now, is the best player
+for unity(better than the new video player).
+I'll create a major version with all youtube features integrated then if you want to upgrade you will be able to pay only the difference.
+
+Support email: kelvinparkour@gmail.com
---
+++ Assets/LiveStreamingExtra.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(MediaPlayerCtrl))]
+[RequireComponent(typeof(YoutubeLivestreamSystem))]
+
+public class LiveStreamingExtra : MonoBehaviour {
+    private YoutubeLivestreamSystem youtubeLiveStream;
+    private MediaPlayerCtrl player;
+
+    public Text debugText;
+
+    private void Start()
+    {
+        youtubeLiveStream = GetComponent<YoutubeLivestreamSystem>();
+        player = GetComponent<MediaPlayerCtrl>();
+    }
+
+    public void Reload()
+    {
+        player.Stop();
+        player.UnLoad();
+        player.m_strFileName = null;
+        youtubeLiveStream.PlayYoutubeVideo(youtubeLiveStream.videoId);
+    }
+
+    private void Update()
+    {
+        debugText.text = player.GetCurrentState().ToString();
+    }
+}
